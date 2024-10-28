61d5509c351a74b542e7d80fcf1525482ace524d
Rucha Oza <ruchadar@asu.edu>
2024-10-27 23:25:58 -0700

    Update sketch.js to make it compatible with colorChange.js; modified to add a color change feature

--- sketch.js
+++ sketch.js
@@ -2,25 +2,24 @@ let circleX, circleY;  // Position of the circle
 let circleRadius = 50; // Radius of the circle
 let dragging = false;  // Is the circle being dragged?
 let offsetX, offsetY;  // Offset to track dragging distance
+let circleColor;
 
 function setup() {
   createCanvas(400, 400);
   // Initial position of the circle
   circleX = width / 2;
   circleY = height / 2;
+  circleColor = color(255, 0, 0);
 }
 
 function draw() {
   background(220);
- 
-  // Draw the circle
-  fill(255, 0, 0);
+  fill(circleColor);
   ellipse(circleX, circleY, circleRadius * 2);
- 
-  // If dragging, update the circle position
+  
   if (dragging) {
     circleX = mouseX + offsetX;
-    circleY = mouseY + offsetY;
+    circleY = mouseY + offestY:
   }
 }
 
@@ -32,6 +31,8 @@ function mousePressed() {
     // Calculate the offset from the circle's center to where the mouse clicked
     offsetX = circleX - mouseX;
     offsetY = circleY - mouseY;
+
+    circleColor = color(random(255), random(255), random(255));
   }
 }
 
